180644e357e841933a75c1c7c2b49560e5ba2648
snaztoz <hafidhmn@gmail.com>
2022-12-14 23:20:53 +0700

    test: add seeder for test

---
+++ database/seeders/User.ts
@@ -0,0 +1,15 @@
+import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+
+import User from 'App/Models/User'
+
+export default class extends BaseSeeder {
+  public static environment = ['test']
+
+  public async run() {
+    await User.create({
+      email: 'alice@email.com',
+      username: 'alice',
+      password: 'password',
+    })
+  }
+}
