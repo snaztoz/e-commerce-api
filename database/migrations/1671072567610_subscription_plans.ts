ef1b1d8d3b3ed76d27832328794b91776662a722
snaztoz <hafidhmn@gmail.com>
2022-12-15 13:15:29 +0700

    style: remove newline

--- database/migrations/1671072567610_subscription_plans.ts
+++ database/migrations/1671072567610_subscription_plans.ts
@@ -6,7 +6,6 @@ export default class extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-
       table.string('name')
       table.decimal('price')
 
