3540adf2a9164cb3d515025eafef566bb895bac8
snaztoz <hafidhmn@gmail.com>
2022-12-15 18:08:54 +0700

    fix: add not null constraint in subscription_plans table

--- database/migrations/1671072567610_subscription_plans.ts
+++ database/migrations/1671072567610_subscription_plans.ts
@@ -6,8 +6,8 @@ export default class extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').primary()
-      table.string('name')
-      table.decimal('price')
+      table.string('name').notNullable()
+      table.decimal('price').notNullable()
 
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
