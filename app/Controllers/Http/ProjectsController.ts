c8d3aa81f166e17fc3a62ec03dd30d89687a0ba5
snaztoz <hafidhmn@gmail.com>
2022-12-18 00:29:34 +0700

    feat: add controller for new project storing

--- app/Controllers/Http/ProjectsController.ts
+++ app/Controllers/Http/ProjectsController.ts
@@ -1,3 +1,4 @@
+import { rules, schema } from '@ioc:Adonis/Core/Validator'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 import ProjectType from 'App/Models/ProjectType'
@@ -7,8 +8,24 @@ export default class ProjectsController {
     return response.notImplemented()
   }
 
-  public async store({ response }: HttpContextContract) {
-    return response.notImplemented()
+  public async store({ auth, request, response }: HttpContextContract) {
+    const user = auth.use('api').user!
+    const newProjectSchema = schema.create({
+      projectTypeId: schema.number([rules.exists({ table: 'project_types', column: 'id' })]),
+    })
+
+    let payload
+    try {
+      payload = await request.validate({ schema: newProjectSchema })
+    } catch (err) {
+      return response.badRequest(err)
+    }
+
+    await user.related('projects').create({
+      projectTypeId: payload.projectTypeId,
+    })
+
+    return response.created()
   }
 
   public async show({ response }: HttpContextContract) {
