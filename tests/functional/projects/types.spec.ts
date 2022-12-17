f2b3692de3b3e459c05c643285736fcd0408a62a
snaztoz <hafidhmn@gmail.com>
2022-12-18 00:22:21 +0700

    test: refactor

--- tests/functional/projects/types.spec.ts
+++ tests/functional/projects/types.spec.ts
@@ -1,17 +1,7 @@
-import Database from '@ioc:Adonis/Lucid/Database'
 import { test } from '@japa/runner'
 
-import ProjectType from 'App/Models/ProjectType'
-
-test.group('Project types', (group) => {
-  group.each.setup(async () => {
-    await Database.beginGlobalTransaction()
-    return () => Database.rollbackGlobalTransaction()
-  })
-
+test.group('Project types', () => {
   test('get project types', async ({ client }) => {
-    await ProjectType.createMany([{ name: 'skripsi' }, { name: 'proposal penelitian' }])
-
     const response = await client.get('/api/projects/types')
 
     response.assertBodyContains({
