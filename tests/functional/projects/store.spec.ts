a0be5dd6cb216af1a5ba4f5b4313b0cd13486964
snaztoz <hafidhmn@gmail.com>
2022-12-18 00:29:15 +0700

    test: add test for storing new project

---
+++ tests/functional/projects/store.spec.ts
@@ -0,0 +1,20 @@
+import { test } from '@japa/runner'
+
+import ProjectType from 'App/Models/ProjectType'
+import User from 'App/Models/User'
+
+test.group('Projects store', () => {
+  test('store new project', async ({ client }) => {
+    const user = await User.findByOrFail('username', 'alice')
+    const skripsi = await ProjectType.findByOrFail('name', 'skripsi')
+
+    const response = await client
+      .post('/api/projects')
+      .json({
+        projectTypeId: skripsi.id,
+      })
+      .loginAs(user)
+
+    response.assertStatus(201)
+  })
+})
