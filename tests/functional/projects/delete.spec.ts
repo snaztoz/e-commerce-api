5cb80fac5a4efac9170f17cf770784e1155df7cc
snaztoz <hafidhmn@gmail.com>
2022-12-19 16:24:59 +0700

    test: fix test cases

--- tests/functional/projects/delete.spec.ts
+++ tests/functional/projects/delete.spec.ts
@@ -5,24 +5,18 @@ import ProjectType from 'App/Models/ProjectType'
 import User from 'App/Models/User'
 
 test.group('Projects delete', (group) => {
-  let alice
-  let skripsi
-  let project
-
   group.each.setup(async () => {
     await Database.beginGlobalTransaction()
-
-    alice = await User.findByOrFail('username', 'alice')
-    skripsi = await ProjectType.findByOrFail('name', 'skripsi')
-    project = await alice.related('projects').create({
-      name: 'Membangun Website dengan AdonisJS',
-      projectTypeId: skripsi.id,
-    })
-
     return () => Database.rollbackGlobalTransaction()
   })
 
   test('delete project', async ({ client }) => {
+    const alice = await User.findByOrFail('username', 'alice')
+    const skripsi = await ProjectType.findByOrFail('name', 'skripsi')
+    const project = await alice.related('projects').create({
+      name: 'Membangun Website dengan AdonisJS',
+      projectTypeId: skripsi.id,
+    })
     const projectId = project.id
 
     const response = await client.delete(`/api/projects/${projectId}`).loginAs(alice)
