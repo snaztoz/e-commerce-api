e237bc4a02591c85fc8736e706b393522279f7e1
snaztoz <hafidhmn@gmail.com>
2022-12-31 15:29:09 +0700

    test: fix test database transaction

--- tests/functional/users/signup.spec.ts
+++ tests/functional/users/signup.spec.ts
@@ -1,6 +1,12 @@
+import Database from '@ioc:Adonis/Lucid/Database'
 import { test } from '@japa/runner'
 
-test.group('Users signup', () => {
+test.group('Users signup', (group) => {
+  group.each.setup(async () => {
+    await Database.beginGlobalTransaction()
+    return () => Database.rollbackGlobalTransaction()
+  })
+
   test('users signup', async ({ client }) => {
     const response = await client.post('/api/users/signup').json({
       username: 'bob',
